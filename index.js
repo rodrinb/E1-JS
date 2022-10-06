debc9b35f1dc6af2d67704e1e8781fa215bc59e9
Rodrigo Nicolas <rodrinb@hotmail.com>
2022-10-06 12:03:31 -0300

    E1-JS-incompleto

---
+++ index.js
@@ -0,0 +1,121 @@
+let pizzas = [
+    {
+        id: 1,
+        Nombre: 'Muzzarela',
+        Ingredientes: 'Salsa de tomate, Queso muzzarela, Oregano',
+        Precio: 1500
+
+    }
+]
+
+console.log(pizzas);
+
+let pizzasGT50 = () => {
+    return pizzas.filter((pizza) => (pizza.prices.medium >= 800))
+
+}
+
+
+
+
+pizzas = [
+    {
+        id: 2,
+        Nombre: 'Fugazza',
+        Ingredientes: 'Cebolla, Queso parmesano',
+        Precio: 500
+
+    }
+]
+
+console.log(pizzas);
+
+let pizzasGT40 = () => {
+    return pizzas.filter((pizza) => (pizza.prices.medium >= 800))
+
+}
+
+
+
+
+
+pizzas = [
+    {
+        id: 3,
+        Nombre: 'Fugazzetta',
+        Ingredientes: 'Cebolla, Queso muzzarela, Salsa de tomate, Oregano',
+        Precio: 1200
+    }
+]
+
+console.log(pizzas);
+
+let pizzasGT30 = () => {
+    return pizzas.filter((pizza) => (pizza.prices.medium >= 800))
+
+}
+
+
+
+pizzas = [
+    {
+        id: 4,
+        Nombre: 'Napolitana',
+        Ingredientes: 'Salsa de tomate, Queso muzzarella, Ajo, Oregano, Aceite de oliva',
+        Precio: 1500
+
+    }
+]
+
+console.log(pizzas);
+
+let pizzasGT20 = () => {
+    return pizzas.filter((pizza) => (pizza.prices.medium >= 800))
+
+}
+
+
+
+pizzas = [
+    {
+        id: 5,
+        Nombre: 'Jamon y Rucula',
+        Ingredientes: 'Salsa de tomate, Queso muzzarella, Jamon crudo, Rucula, Oregano',
+        Precio: 1600
+
+    }
+]
+
+console.log(pizzas);
+
+let pizzasGT10 = () => {
+    return pizzas.filter((pizza) => (pizza.prices.medium >= 800))
+
+}
+
+
+
+pizzas = [
+    {
+        id: 6,
+        Nombre: 'Caprese',
+        Ingredientes: 'Salsa de tomate, Tomate en rodajas, Queso muzzarela, Albahaca',
+        Precio: 1600
+
+    }
+]
+
+console.log(pizzas);
+
+let pizzasGT00 = () => {
+    return pizzas.filter((pizza) => (pizza.prices.medium >= 800))
+
+}
+
+
+
+
+
+
+
+
